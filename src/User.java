917b43e3a8537669135206bf622182c77b767126
Yavor <d69@abv.bg>
2022-10-19 10:20:00 +0300

    some little improvements

--- src/User.java
+++ src/User.java
@@ -15,52 +15,46 @@ public class User {
     }
 
     public void removeTask() {
-        if (!tasks.isEmpty()) {
+        if (!this.tasks.isEmpty()) {
             System.out.println("Enter the number of the task to be removed ... ");
-            tasks.remove(inputNumberOfTask()-1);
+            this.tasks.remove(inputNumberOfTask()-1);
         } else System.out.println("Your TODO list is empty.");
     }
 
     public void changeTaskTitle() {
-        if (!tasks.isEmpty()) {
+        if (!this.tasks.isEmpty()) {
             int num = getNumOfTaskToChange();
-            Task task = tasks.get(num);;
-            task.changeTitle();
-            tasks.set(num, task);
+            this.tasks.get(num).changeTitle();
         } else System.out.println("Your TODO list is empty.");
     }
 
     public void changeTaskDescription() {
-        if (!tasks.isEmpty()) {
+        if (!this.tasks.isEmpty()) {
             int num = getNumOfTaskToChange();
-            Task task = tasks.get(num);;
-            task.changeDescription();
-            tasks.set(num, task);
+            this.tasks.get(num).changeDescription();
         } else System.out.println("Your TODO list is empty.");
     }
 
     public void changeTaskPriority() {
-        if (!tasks.isEmpty()) {
+        if (!this.tasks.isEmpty()) {
             int num = getNumOfTaskToChange();
-            Task task = tasks.get(num);;
-            task.changePriority();
-            tasks.set(num, task);
+            this.tasks.get(num).changePriority();
         } else System.out.println("Your TODO list is empty.");
     }
 
     public void swapTasks() {
-        if(tasks.isEmpty()) System.out.println("Your TODO list is empty.");
-        else if (tasks.size()==1) System.out.println("Your TODO list has just one task.");
+        if(this.tasks.isEmpty()) System.out.println("Your TODO list is empty.");
+        else if (this.tasks.size()==1) System.out.println("Your TODO list has just one task.");
         else {
             System.out.println("Enter the numbers of the tasks to be swapped ... ");
             int n1 = inputNumberOfTask();
             int n2 = inputNumberOfTask();
-            Collections.swap(tasks, (n1-1), (n2-1));
+            Collections.swap(this.tasks, (n1-1), (n2-1));
         }
     }
 
     public void checkWhetherIsEmpty() {
-        if(tasks.isEmpty()) System.out.println("Your TODO list is empty.");
+        if(this.tasks.isEmpty()) System.out.println("Your TODO list is empty.");
         else System.out.println("Your TODO list is not empty.");
     }
 
@@ -71,7 +65,7 @@ public class User {
             sc.next();
         }
         int num = sc.nextInt();
-        while (num < 1 || num > tasks.size()) {
+        while (num < 1 || num > this.tasks.size()) {
             System.out.println("Invalid input number. Try again!");
             num = inputNumberOfTask();
         }
@@ -84,11 +78,11 @@ public class User {
     }
 
     public void printTodoList() {
-        if(tasks.isEmpty()) System.out.println("Your TODO list is empty.");
+        if(this.tasks.isEmpty()) System.out.println("Your TODO list is empty.");
         else {
-            System.out.println("User " + password + "'s TODO list: ");
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i+1) + ".  " + tasks.get(i));
+            System.out.println("Your TODO list is: ");
+            for (int i = 0; i < this.tasks.size(); i++) {
+                System.out.println((i+1) + ".  " + this.tasks.get(i));
             }
         }
     }
